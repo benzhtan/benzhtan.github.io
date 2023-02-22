18ef7b2463dd9c109124f78fdc0e94c2ad36894c
“Benjamin <ben.zh.tan@gmail.com>
2023-02-21 20:28:23 -0500

    Add to 02-21 scripts

--- homework/02-21/script.js
+++ homework/02-21/script.js
@@ -1,13 +1,19 @@
-d3.csv("/data/gapminder.csv").then(function(data)) {
+d3.csv("/data/gapminder.csv").then(function(data) {
 // 1. Create a SVG viewport where the chart will appear//
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 console.log(window.innerWidth);
 console.log(document.querySelector("#chart")).clientWidth;
+
+const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
 // 2. Filter the data                                  //
 // 3. Determine x-y axes                               //
 // 4. Draw axes                                        //
 // 5. Draw bars                                        //
 // 6. Draw axis lables                                 //
-};
+});
