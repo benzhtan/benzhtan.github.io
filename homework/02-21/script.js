361d70a002887764289477bbeca1d36497b549be
“Benjamin <ben.zh.tan@gmail.com>
2023-02-28 16:45:02 -0500

    Updated assignment landing page; added to homework

--- homework/02-21/script.js
+++ homework/02-21/script.js
@@ -1,4 +1,5 @@
 d3.csv("/data/gapminder.csv").then(function(data) {
+
 // 1. Create a SVG viewport where the chart will appear//
 const width = document.querySelector("#chart").clientWidth;
 const height = document.querySelector("#chart").clientHeight;
@@ -23,10 +24,54 @@ const lifeExp = {
     max: d3.max(filtered_data, function(d) {return +d.lifeExp; })
 };
 
-console.log(lifeExp.min, lifeExp.max)
-
 // 4. Determine x-y axes //
+
+const margin = {top: 50, left: 100, right: 50, bottom: 100};
+
+const xScale = d3.scaleBand()
+    .domain(["1952", "1957", "1962", "1967", "1972", "1977", "1982", "1987", "1992", "1997", "2002", "2007"])
+    .range([margin.left, width - margin.right])
+    .padding(0.5);
+
+const yScale = d3.scaleLinear()
+    .domain([50, lifeExp.max])
+    .range([height - margin.bottom, margin.top]);
+
 // 5. Draw axes //
+
+const xAxis = svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", `translate(${height - margin.bottom})`)
+    .call(d3.axisBottom().scale(xScale));
+
+const yAxis = svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft().scale(yScale));
+
 // 6. Draw bars //
+
+const points = svg.selectAll("rect")
+    .data(filtered_data)
+    .enter()
+    .append("rect")
+        .attr("x", function(d) {return xScale(d.year);})
+        .attr("y", function(d) {return yScale(d.leftExp);})
+        .attr("width", xScale.bandWidth())
+        .attr("height", function(d) {return height - (margin.bottom)})
+
 // 7. Draw axis lables //
+
+const xAxisLabel = svg.append("text")
+    .attr("class","axisLabel")
+    .attr("x", width/2)
+    .attr("y", height-margin.bottom/2)
+    .text("Year");
+
+const yAxisLabel = svg.append("text")
+    .attr("class","axisLabel")
+    .attr("transform","rotate(-90)")
+    .attr("x", -height/2)
+    .attr("y", margin.left/2)
+    .text("Life Expectancy (Years)");
 });
