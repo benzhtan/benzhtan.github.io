877194c4d3e7e090da6c50b657f11ea65d3a98b3
“Benjamin <ben.zh.tan@gmail.com>
2023-02-28 17:06:10 -0500

    added curly braces to fix undeclared vars

--- homework/02-21/script.js
+++ homework/02-21/script.js
@@ -1,77 +1,92 @@
-d3.csv("/data/gapminder.csv").then(function(data) {
+d3.csv("./data/gapminder.csv").then(function(data) {
 
-// 1. Create a SVG viewport where the chart will appear//
-const width = document.querySelector("#chart").clientWidth;
-const height = document.querySelector("#chart").clientHeight;
+    //1. DEFINE DIMENSIONS OF SVG + CREATE SVG CANVAS//
 
-// Initializing viewport of SVG canvas //
-const svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
 
-// 2. Filter the data //
+    const width = document.querySelector("#chart").clientWidth;
+    const height = document.querySelector("#chart").clientHeight;
 
-// Initializing new variable //
-let filtered_data = data.filter(function(d) {
-    return d.country === "United States";
-});
+    // Initializing the viewport of the SVG canvas//
+    // An SVG Canvas's Viewport has a "width" and "height"//
+    
+    const svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-// 3. Determine min and max values //
 
-const lifeExp = {
-    min: d3.min(filtered_data, function(d) {return +d.lifeExp; }),  //the "+" forces str to int //
-    max: d3.max(filtered_data, function(d) {return +d.lifeExp; })
-};
+    //2. FILTER THE DATA //
 
-// 4. Determine x-y axes //
+    let filtered_data = data.filter(function(d) {
+        return d.country === 'United States';
+    });
 
-const margin = {top: 50, left: 100, right: 50, bottom: 100};
+    // 3. DETERMINE MIN AND MAX VALUES OF VARIABLES //
 
-const xScale = d3.scaleBand()
-    .domain(["1952", "1957", "1962", "1967", "1972", "1977", "1982", "1987", "1992", "1997", "2002", "2007"])
-    .range([margin.left, width - margin.right])
-    .padding(0.5);
+    const lifeExp = {
+        min: d3.min(filtered_data, function(d) { return +d.lifeExp; }),
+        max: d3.max(filtered_data, function(d) { return +d.lifeExp; })
+    };
 
-const yScale = d3.scaleLinear()
-    .domain([50, lifeExp.max])
-    .range([height - margin.bottom, margin.top]);
+    // 4. CREATE SCALES //
 
-// 5. Draw axes //
+    const margin = {
+        top: 50, 
+        left: 100, 
+        right: 50, 
+        bottom: 100
+    };
 
-const xAxis = svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", `translate(${height - margin.bottom})`)
-    .call(d3.axisBottom().scale(xScale));
+    const xScale = d3.scaleBand()
+        .domain(["1952","1957","1962","1967","1972","1977","1982","1987","1992","1997","2002","2007"])
+        .range([margin.left, width - margin.right])
+        .padding(0.5);
 
-const yAxis = svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft().scale(yScale));
+    const yScale = d3.scaleLinear()
+        .domain([50, lifeExp.max])
+        .range([height - margin.bottom, margin.top]);
 
-// 6. Draw bars //
 
-const points = svg.selectAll("rect")
-    .data(filtered_data)
-    .enter()
-    .append("rect")
-        .attr("x", function(d) {return xScale(d.year);})
-        .attr("y", function(d) {return yScale(d.leftExp);})
-        .attr("width", xScale.bandWidth())
-        .attr("height", function(d) {return height - (margin.bottom)})
+    // 5. DRAW AXES //
+    
+    const xAxis = svg.append("g")
+        .attr("class","axis")
+        .attr("transform", `translate(0,${height-margin.bottom})`)
+        .call(d3.axisBottom().scale(xScale));
 
-// 7. Draw axis lables //
+    const yAxis = svg.append("g")
+        .attr("class","axis")
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft().scale(yScale));
 
-const xAxisLabel = svg.append("text")
-    .attr("class","axisLabel")
-    .attr("x", width/2)
-    .attr("y", height-margin.bottom/2)
-    .text("Year");
 
-const yAxisLabel = svg.append("text")
-    .attr("class","axisLabel")
-    .attr("transform","rotate(-90)")
-    .attr("x", -height/2)
-    .attr("y", margin.left/2)
-    .text("Life Expectancy (Years)");
+    // 6. DRAW BARS //
+
+    const points = {svg.selectAll("rect")
+        .data(filtered_data)
+        .enter()
+        .append("rect")
+            .attr("x", function(d) { return xScale(d.year); })
+            .attr("y", function(d) { return yScale(d.lifeExp); })
+            .attr("width", xScale.bandwidth())
+            .attr("height", function(d) { return height - (margin.bottom + yScale(d.lifeExp)) })
+            .attr("fill", "steelblue");
+    }
+    
+    // 7. DRAW AXIS LABELS //
+    
+    const xAxisLabel = {svg.append("text")
+        .attr("class","axisLabel")
+        .attr("x", width/2)
+        .attr("y", height-margin.bottom/2)
+        .text("Year");
+        }
+
+    const yAxisLabel = {svg.append("text")
+                           .attr("class","axisLabel")
+                           .attr("transform","rotate(-90)")
+                           .attr("x", -height/2)
+                           .attr("y", margin.left/2)
+                           .text("Life Expectancy (Years)");
+                        }
 });
